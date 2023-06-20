43527c116663c7bd8d9353dfd06bb662b2934d4c
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-06-20 18:00:29 +0200

    added class

--- src/components/customdiv/SwirlDiv.vue
+++ src/components/customdiv/SwirlDiv.vue
@@ -19,36 +19,38 @@
 <template>
   <div class="clipdiv">
     <slot> </slot>
-    <svg height="0" width="0">
-      <defs>
-        <clipPath id="clip" clipPathUnits="objectBoundingBox">
-          <path
-            d="M 0,1
+    <div class="clipcontainer">
+      <svg height="0" width="0">
+        <defs>
+          <clipPath id="clip" clipPathUnits="objectBoundingBox">
+            <path
+              d="M 0,1
               L 0,0
               L 1,0
               L 1,1
               C .65 .8, .35 .9, 0 0.95
               Z"
-          />
-        </clipPath>
-      </defs>
-    </svg>
+            />
+          </clipPath>
+        </defs>
+      </svg>
 
-    <div class="border-1"></div>
-    <div class="border-2"></div>
-    <div class="border-3"></div>
+      <div class="border-1"></div>
+      <div class="border-2"></div>
+      <div class="border-3"></div>
 
-    <svg height="0" width="0">
-      <defs>
-        <clipPath id="clip2" clipPathUnits="objectBoundingBox">
-          <path
-            d="M0,0.9
+      <svg height="0" width="0">
+        <defs>
+          <clipPath id="clip2" clipPathUnits="objectBoundingBox">
+            <path
+              d="M0,0.9
     C .5 .9, .45 .75, 1 0.95 L1 1 L0 1
     Z"
-          />
-        </clipPath>
-      </defs>
-    </svg>
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
   </div>
 </template>
 
