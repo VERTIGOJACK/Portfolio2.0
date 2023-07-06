49e298dc1049ba57daa54b1d4e92fac150091263
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-07-06 20:41:07 +0200

    stash

--- src/views/software/softwareitem/SoftwareItemContent.vue
+++ src/views/software/softwareitem/SoftwareItemContent.vue
@@ -1,60 +1,60 @@
 <script setup>
-import Carousel from "../../../components/carousel/Carousel.vue";
-import { getDataList } from "../functions";
-import { ref, onMounted } from "vue";
+  import Carousel from "../../../components/carousel/Carousel.vue";
+  import { getDataList } from "../functions";
+  import { ref, onMounted } from "vue";
 
-const classArray = ["blue", "red", "yellow"];
+  const classArray = ["blue", "red", "yellow"];
 
-let counter = 0;
+  let counter = 0;
 
-const CountClass = () => {
-  let mClass = counter;
-  counter >= 2 ? (counter = 0) : counter++;
-  return classArray[mClass];
-};
+  const CountClass = () => {
+    let mClass = counter;
+    counter >= 2 ? (counter = 0) : counter++;
+    return classArray[mClass];
+  };
 
-let content = ref({
-  featuredImage: "/VDD.svg",
-  technologies: [
-    "Vue",
-    "VS",
-    "Three.js",
-    "Vue",
-    "VS",
-    "Three.js",
-    "Vue",
-    "VS",
-    "Three.js",
-  ],
-  sectionTitle: "Title",
-  sectionText: "description lorem lorem lorem lorem lorem lorem lorem",
-  carouselList: [
-    {
-      url: "/VDD.svg",
-      title: "",
-      text: "",
+  let content = ref({
+    featuredImage: "/VDD.svg",
+    technologies: [
+      "Vue",
+      "VS",
+      "Three.js",
+      "Vue",
+      "VS",
+      "Three.js",
+      "Vue",
+      "VS",
+      "Three.js",
+    ],
+    sectionTitle: "Title",
+    sectionText: "description lorem lorem lorem lorem lorem lorem lorem",
+    carouselList: [
+      {
+        url: "/VDD.svg",
+        title: "",
+        text: "",
+      },
+    ],
+    link: {
+      text: "Button Text",
+      url: "Loading...",
     },
-  ],
-  link: {
-    text: "Button Text",
-    url: "Loading...",
-  },
-  version: "1.0",
-  platforms: "Web",
-});
+    version: "1.0",
+    platforms: "Web",
+  });
 
-const changeClass = (e) => {
-  const test = document.createElement("div");
-  e.target.classList.toggle("rainbow-border");
-};
+  const changeClass = (e) => {
+    const test = document.createElement("div");
+    e.target.classList.toggle("rainbow-border");
+  };
 
-// const apiData = await Promise.all(()=>{
-//   const res = fetch();
+  // const apiData = await Promise.all(()=>{
+  //   const res = fetch();
 
-// });
+  // });
 
-// const mydata = await getData();
-// softwareContent.value.items = mydata.reverse();
+  // const mydata = await getData();
+  // softwareContent.value.items = mydata.reverse();
 </script>
 
 <template>
@@ -68,8 +68,7 @@ const changeClass = (e) => {
       <p
         v-for="item in content.technologies"
         class="technology"
-        :class="CountClass()"
-      >
+        :class="CountClass()">
         {{ item }}
       </p>
     </div>
@@ -91,51 +90,51 @@ const changeClass = (e) => {
 </template>
 
 <style scoped>
-h1 {
-  margin-bottom: 0;
-  margin-top: 0;
-}
-.title {
-  margin-top: var(--lengths-md-3);
-  display: flex;
-  justify-content: start;
-  align-items: center;
-}
-.title img {
-  height: 3rem;
-  aspect-ratio: 1/1;
-  border-radius: 100%;
-  margin-right: var(--lengths-sm-2);
-}
+  h1 {
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+  .title {
+    margin-top: var(--lengths-md-3);
+    display: flex;
+    justify-content: start;
+    align-items: center;
+  }
+  .title img {
+    height: 3rem;
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+    margin-right: var(--lengths-sm-2);
+  }
 
-.info {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.technologies {
-  display: flex;
-  gap: var(--lengths-sm-2);
-  flex-wrap: wrap;
-  margin-bottom: 1rem;
-}
-.technology {
-  border-radius: var(--lengths-sm-2);
-  padding: var(--lengths-sm-1);
-  color: var(--monochrome-1);
-  margin: 0;
-}
-.technology * {
-  margin: 0;
-  padding: 0;
-}
-.blue {
-  background-color: var(--accent-1);
-}
-.red {
-  background-color: var(--accent-2);
-}
-.yellow {
-  background-color: var(--accent-3);
-}
+  .info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .technologies {
+    display: flex;
+    gap: var(--lengths-sm-2);
+    flex-wrap: wrap;
+    margin-bottom: 1rem;
+  }
+  .technology {
+    border-radius: var(--lengths-sm-2);
+    padding: var(--lengths-sm-1);
+    color: var(--monochrome-1);
+    margin: 0;
+  }
+  .technology * {
+    margin: 0;
+    padding: 0;
+  }
+  .blue {
+    background-color: var(--accent-1);
+  }
+  .red {
+    background-color: var(--accent-2);
+  }
+  .yellow {
+    background-color: var(--accent-3);
+  }
 </style>
